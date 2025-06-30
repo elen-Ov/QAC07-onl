5d86466a570e26080608354bca0505f8cb9b90e0
tikaum <tulkubai@rambler.ru>
2025-07-01 00:16:48 +0500

    Homework (try#5) for lesson 3, Kayumov

---
+++ Lesson 3/Task2_Kayumov/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task2_Kayumov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10] { 3, 7, -4, 2, 8, 1, 5, 6, 9, -10 };
+
+            int sumEven = 0, quantityUneven = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEven = sumEven + numbers[i];
+                }
+                else
+                {
+                    quantityUneven = quantityUneven + 1;
+                }
+            }
+
+            Console.WriteLine($"The sum of even numbers is {sumEven}");
+            Console.WriteLine($"The number of odd numbers is {quantityUneven}");
+        }
+    }
+}
---
+++ Lesson 3/Task3_Kayumov/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task3_Kayumov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+            int sumGrades = 0;
+
+            Console.WriteLine("Enter grades for 5 subjects (0 to 100)");
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                sumGrades = sumGrades + grades[i];
+            }
+
+            double averageRating = sumGrades / grades.Length;
+
+            if (averageRating >= 90)
+            {
+                Console.WriteLine("Отлично");
+            }
+
+            else if (averageRating <= 89  && averageRating >= 75)
+            {
+                Console.WriteLine("Хорошо");
+            }
+
+            else if (averageRating <= 74 && averageRating >= 60)
+            {
+                Console.WriteLine("Удовлетворительно");
+            }
+
+            else if (60 > averageRating)
+            {
+                Console.WriteLine("Неудовлетворительно");
+            }
+        }
+    }
+}
