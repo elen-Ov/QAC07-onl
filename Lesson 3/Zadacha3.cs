c5d569490bde099899e557d992ec0d23240e6507
Yulia <yulemona7@gmail.com>
2025-07-02 23:23:30 +0300

    Zadachi

---
+++ Lesson 3/Zadacha3.cs
@@ -0,0 +1,42 @@
+﻿    using System.Diagnostics.CodeAnalysis;
+    using System.Net.Quic;
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Enter mark" + (i + 1));
+        sum += Convert.ToInt32(Console.ReadLine());
+    }
+    int average = sum / 5;
+
+    if (average > 100)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        Console.WriteLine(average);
+
+        switch (average)
+        {
+            case >= 90:
+                Console.WriteLine("Отлично");
+                break;
+
+            case >= 75:
+                Console.WriteLine("Хорошо");
+                break;
+
+            case >= 60:
+                Console.WriteLine("Удовлетворительно");
+                break;
+
+            default:
+                Console.WriteLine("Неудовлетворительно");
+                break;
+        }
+    }
+
+
+
+
