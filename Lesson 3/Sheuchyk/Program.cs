83c9a7df41a11d12aca6cc8ebcdaf7b9c009c4e0
Alena Sheuchyk <asheuchyk@AM.IDTCORP.NET>
2025-07-03 15:10:10 +0300

    Переместила задание в Lesson 3/Sheuchyk

---
+++ Lesson 3/Sheuchyk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Task #1
+        Console.Write("Enter the deposit amount: ");
+        decimal deposit = decimal.Parse(Console.ReadLine());
+
+     
+        decimal rate = 0.10m;
+
+        // After 1 year
+        deposit = deposit + (deposit * rate);
+        Console.WriteLine("Amount after year 1: " + deposit);
+
+        // After 2 years
+        deposit = deposit + (deposit * rate);
+        Console.WriteLine("Amount after year 2: " + deposit);
+
+        // After 3 years
+        deposit = deposit + (deposit * rate);
+        Console.WriteLine("Amount after year 3: " + deposit);
+
+      
+        Console.WriteLine("Press any key to exit...");
+    
+    }
+}
