00b5cba9fdefa8caba1f3390fe3190406c9b5021
Pavel <pawellm96@gmail.com>
2025-07-06 16:34:29 +0300

    Ansers on PR coments for task 1

--- Lesson 3/Matsiyevich/Task1_deposite calc/Program.cs
+++ Lesson 3/Matsiyevich/Task1_deposite calc/Program.cs
@@ -4,15 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //Затача №1
             Console.WriteLine("Можно вводить любые значения, но чтобы программа соответствовала ДЗ, пожалуйста введите 10 для процентов и 3 для лет пользования депозита");
             Console.WriteLine("Enter precents: ");
 
-            string precentEntered = Console.ReadLine();
-            double precetnts = Convert.ToInt32(precentEntered);
-            double precentsByDeposit = precetnts / 100;
+            string percentEntered = Console.ReadLine();
+            double percents = Convert.ToInt32(percentEntered);
+            double percentsByDeposit = percents / 100;
 
-            Console.WriteLine($"Your annual interest on deposit: {precetnts}%");
+            Console.WriteLine($"Your annual interest on deposit: {percents}%");
 
             Console.WriteLine("Enter deposit: ");
 
@@ -31,9 +30,9 @@
                 Console.Write("Enter positive Integer: ");
             }
 
-            for (int i = 1; i <= Convert.ToInt32(years); i++)
+            for (int i = 1; i <= years; i++)
             {
-                deposit += precentsByDeposit * deposit;
+                deposit += percentsByDeposit * deposit;
                 Console.WriteLine($"{i}. Your profit will be {deposit}");
             }
             Console.WriteLine($"After {years} years your deposit will increase to {deposit}");
